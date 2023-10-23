a24e968d11955d125e84cb7986a8b24492fd36c8
Alexander Ryzhov <github@ryzhov.su>
2023-10-23 20:48:42 +0300

    Battery capacity baseline rule

---
+++ Configuration Items/CI-BatteryCapacity.ps1
@@ -0,0 +1,15 @@
+﻿<#
+    .SYNOPSIS
+        Get remaining battery capacity
+    .DESCRIPTION
+        Returns percentage (integer number) of designed battery capacity.
+    .NOTES
+        As for me, my compliance rule is set to fail if it falls below 30%.
+        Part of device health baseline. 
+    .LINK
+        https://learn.microsoft.com/en-us/windows/win32/cimwin32prov/win32-battery
+        Ryzhov Alexander <github@ryzhov.su> 
+        https://github.com/ryzhovau
+#>
+
+return [int]((Get-WmiObject -Namespace root/wmi -Class BatteryFullChargedCapacity).FullChargedCapacity / (Get-WmiObject -Namespace root/wmi -Class BatteryStaticData).DesignedCapacity * 100)
